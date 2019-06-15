d786b17d8f805a71368ab8436c3e309eba5265dd
Rafael <rafaelminossi@rede.ulbra.br>
2019-06-15 08:39:23 -0300

    Criação de componente da Homepage

---
+++ lib/tabs/home_tab.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget _buildBodyBack() => Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 76, 158, 101),
+            Color.fromARGB(255, 27, 192, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+        )
+      ),
+    );
+    return Stack(
+      children: <Widget>[
+        _buildBodyBack(),
+        CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              floating: true,
+              snap: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0.0,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text("Dashboard"),
+                centerTitle: true,
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
